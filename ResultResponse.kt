4fa32615efa2cd19f60c3976dee02c34edec5dc8
Dor Shitrit <dor.shitrit@me.com>
2019-06-05 12:20:30 +0300

    Update ResultResponse.kt

--- ResultResponse.kt
+++ ResultResponse.kt
@@ -17,3 +17,31 @@ sealed class ResultResponseCode {
     object CodeSuccess : ResultResponseCode()
     object CodeFailure : ResultResponseCode()
 }
+
+
+
+fun <T : Any> Call<ResultResponseModel<T>>.enqueue(
+    serverSuccess: (response: ResultResponseModel<T>) -> Unit,
+    serverFailure: (t: String) -> Unit
+) {
+    enqueue(object : Callback<ResultResponseModel<T>> {
+        override fun onResponse(call: Call<ResultResponseModel<T>>, response: Response<ResultResponseModel<T>>) {
+            if (response.isSuccessful) {
+                response.body()?.let { result ->
+                    if (result.code == 1) { //server success - positive
+                        serverSuccess(result.copy(resultResponseCode = ResultResponseCode.CodeSuccess))
+                    } else { //server success - negative
+                        serverSuccess(result.copy(resultResponseCode = ResultResponseCode.CodeFailure))
+                    }
+                }
+            } else { //server failure
+                serverFailure("Error Connection")
+            }
+        }
+
+        override fun onFailure(call: Call<ResultResponseModel<T>>, t: Throwable) {
+            serverFailure(t.message ?: "")  //server failure
+            t.printStackTrace()
+        }
+    })
+}
